e1a09edfe99e58b817c052441b8bcc2fb8e46805
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2021-12-09 14:33:30 +0200

    Car Admin Customization

--- cars/admin.py
+++ cars/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from django.contrib.admin.sites import site
+from django.utils.html import format_html
 from .models import Car
-# Register your models here.
 
 
-admin.site.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="40" style="border-radius: 50px;" />'.format(object.car_photo.url))
+
+    thumbnail.short_description = "Car Photo"
+    
+    list_display = ('id', 'thumbnail', 'car_title', 'city', 'color', 'model', 'year', 'body_style', 'fuel_type', 'is_featured')
+    list_display_links = ('id', 'thumbnail','car_title')
+    list_editable = ('is_featured',)
+    search_fields = ('id', 'car_title', 'city', 'model')
+    list_filter = ('city', 'model', 'body_style', 'fuel_type')
+
+admin.site.register(Car, CarAdmin)
