1ca8228ab60d6c686e78ff7f510e331b3daf9ee2
AbdElMegeed-7 <AbdElMegeedX7@gmail.com>
2021-12-16 15:37:17 +0200

    User Login Functionality

--- accounts/views.py
+++ accounts/views.py
@@ -3,10 +3,24 @@ from django.contrib.auth.models import User
 from django.contrib import messages, auth
 
 def login(request):
-  return render(request, 'accounts/login.html')
+    if request.method == 'POST' :
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You are now Loggid in ")
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login Credentials ')
+            return redirect('login')
+        
+    return render(request, 'accounts/login.html')
 
 def register(request):
-  if request.method == 'POST':
+    if request.method == 'POST':
         firstname = request.POST['firstname']
         lastname = request.POST['lastname']
         username = request.POST['username']
@@ -14,39 +28,31 @@ def register(request):
         password = request.POST['password']
         confirm_password = request.POST['confirm_password']
       
-      if password == confirm_password:
-        if User.objects.filter(username=username).exists():        
-            messages.error(request, 'Username Already Exists')
-            return redirect('register')
+        if password == confirm_password:
         
-        else:
-          
-          if User.objects.filter(email=email).exists():
-            messages.error(request, 'Email Already Exists')
-            return redirect('register')
-          else:
-            user = User.objects.create_user(
-                                          first_name=firstname,
-                                          last_name=lastname,
-                                          email=email,
-                                          username=username,
-                                          password=password,
-                                          )
-            auth.login(request, user)
-            messages.success(request, 'You are now LoggidIn')
-            return redirect('dashboard')
-            user.save()
-            messages.success(request, "You Are Registered Successfully")
-            return redirect('login')
+            if User.objects.filter(username=username).exists():        
+                messages.error(request, 'Username Already Exists')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email Already Exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=firstname, last_name=lastname, email=email, username=username, password=password)
+                    auth.login(request, user)
+                    messages.success(request, 'You are now LoggidIn')
+                    return redirect('dashboard')
+                    user.save()
+                    messages.success(request, "You Are Registered Successfully")
+                    return redirect('login')
           
       
-      else:
-        messages.error(request, 'Password Do Not Match')
-        return redirect('register')
-        
-
-  else:
-      return render(request, 'accounts/register.html')
+        else:
+            messages.error(request, 'Password Do Not Match')
+            return redirect('register')
+          
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def dashboard(request):
