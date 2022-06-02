68a6f905f1485cceaecfce128014153ac4dad785
AbdElMegeed <AbdElMegeedX7@gmail.com>
2022-06-02 22:39:32 +0400

    CommiT

--- carzone/settings.py
+++ carzone/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'django-insecure-wx*j6c&re7kpvosain9h!p&)f68%2rpk#()@r-545^q$@o1!zi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['carzoone.herokuapp.com']
 
 
 # Application definition
@@ -187,41 +187,41 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 LOGGING = {
-'version': 1,
-'disable_existing_loggers': False,
-'formatters': {
-    'verbose': {
-        'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
-                   'pathname=%(pathname)s lineno=%(lineno)s '
-                   'funcname=%(funcName)s %(message)s'),
-        'datefmt': '%Y-%m-%d %H:%M:%S'
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
     },
-    'simple': {
-        'format': '%(levelname)s %(message)s'
-    }
-},
-'handlers': {
-    'null': {
-        'level': 'DEBUG',
-        'class': 'logging.NullHandler',
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
     },
-    'console': {
-        'level': 'INFO',
-        'class': 'logging.StreamHandler',
-        'formatter': 'verbose'
-    }
-},
-'loggers': {
-    'django': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-        'propagate': True,
-    },
-    'django.request': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
-        'propagate': False,
-    },
-}}
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }}
 
 # django_heroku.settings(config=locals(), staticfiles=False,logging=False)
