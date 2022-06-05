83995f9284c7953ee733506f8e0fedd6eb6171ef
AbdElMegeed <AbdElMegeedX7@gmail.com>
2022-06-05 15:43:08 +0400

    Fix DEBUG=False   CSRF_TRUSTED_ORIGINS

--- carzone/settings.py
+++ carzone/settings.py
@@ -19,7 +19,8 @@ SECRET_KEY = 'django-insecure-wx*j6c&re7kpvosain9h!p&)f68%2rpk#()@r-545^q$@o1!zi
 DEBUG = True
 
 ALLOWED_HOSTS = ['carzoone.herokuapp.com', '127.0.0.1:8000']
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://carzoone.herokuapp.com', 'https://127.0.0.1:8000']
 
 # Application definition
 
@@ -181,45 +182,6 @@ EMAIL_USE_TLS = True
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
-                       'pathname=%(pathname)s lineno=%(lineno)s '
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }}
 
 # django_heroku.settings(config=locals(), staticfiles=False,logging=False)
 django_heroku.settings(locals())
